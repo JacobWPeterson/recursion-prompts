909bc9dff69b82f76631430b94b70e0e2e447422
Jacob Peterson <petersonjacobw@gmail.com>
2020-12-26 20:06:59 -0800

    Solved 9/10

--- src/recursion.js
+++ src/recursion.js
@@ -117,10 +117,19 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 0) return false;
+  if (n === 1) return true;
+  if (n < 1) return false;
+  return powerOfTwo(n/2)
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 1) {
+    return string[0];
+  } else {
+    return string[string.length-1] + reverse(string.substring(0, string.length-1));
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
